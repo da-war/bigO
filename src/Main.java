76b1c1e2d94a42adbdb8a1cf2bb7c187ea1e3142
drkast <imdawar090@gmail.com>
2024-09-21 04:01:18 +0500

    Constant time

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+//        O(1) time complexity because it's running in the constant time
         System.out.println("Hello world!");
-
+        System.out.println("hello world 2");
 
     }
 }
