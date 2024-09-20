ddb47b156d2d5eaaf75a24026ed08f6a1b188cb3
drkast <imdawar090@gmail.com>
2024-09-21 04:06:46 +0500

    O(n) time complexity algo

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-//        O(1) time complexity because it's running in the constant time
-        System.out.println("Hello world!");
-        System.out.println("hello world 2");
-
+//        0(n) time complexity because it's running numbers.length time
+        int[] numbers= {1,2,3,4,5};
+        for (int i=0;i<numbers.length;i++){
+            System.out.println(numbers[i]);
+        }
     }
 }
+
+
+
+//        O(1) time complexity because it's running in the constant time
+//        System.out.println("Hello world!");
+//        System.out.println("hello world 2");
