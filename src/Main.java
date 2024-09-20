687428b0cfa388aac22c64baa3e29c6298de76df
drkast <imdawar090@gmail.com>
2024-09-21 04:18:15 +0500

    Second exception

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-//        0(n) time complexity because it's running numbers.length time
-        //by adding two print statements the time complexity should be O(2+n) but in such cases we drop the constants and it becomes O(n)
-        System.out.println("something");
+
+
+        //if we add two loops then the time complexity should be O(2n) but we drop the constant in this case too
         int[] numbers= {1,2,3,4,5};
         for (int number :numbers){
             System.out.println(number);
+        }for (int number :numbers){
+            System.out.println(number);
         }
-        System.out.println("something");
     }
-}
+    }
 
 
-
+//by adding two print statements the time complexity should be O(2+n) but in such cases we drop the constants and it becomes O(n)
+//        System.out.println("something");
+//        int[] numbers= {1,2,3,4,5};
+//        for (int number :numbers){
+//           System.out.println(number);
+//        }
+//                System.out.println("something");
 //        O(1) time complexity because it's running  in the constant time
 //        System.out.println("Hello world!");
 //        System.out.println(" hello world 2");
